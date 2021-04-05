e13d11d7f28ecebe19d61bf3c780187189258e1a
Raul P <rpjimenez@spartan-sys.com>
2021-04-05 12:04:59 +0200

    Increase to v0.1.6: Added navbar auto hide with scroll down.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'El Tablón de Clubhouse';
+
+  ngOnInit() {
+    let prevScrollpos = document.getElementById('mainContainer').scrollTop;
+    let upScrolling = 0;
+
+    document.getElementById('mainContainer').onscroll = () => {
+      const currentScrollPos = document.getElementById('mainContainer').scrollTop;
+
+      if (prevScrollpos > currentScrollPos) {
+        upScrolling += 1;
+      } else if (currentScrollPos > 60) {
+        document.getElementById('sticky-navbar').classList.add('d-none');
+      }
+
+      if (upScrolling > 12 || currentScrollPos === 0) {
+        document.getElementById('sticky-navbar').classList.remove('d-none');
+        upScrolling = 0;
+      }
+
+      prevScrollpos = currentScrollPos;
+    };
+  }
+
 }
